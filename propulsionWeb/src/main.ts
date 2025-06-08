341592d22612f85a1b80a801924d47561b471e37
Jan Vlietland <j.vlietland@search4solutions.nl>
2025-06-08 19:08:57 +0200

    update assetPaths without comments

--- propulsionWeb/src/main.ts
+++ propulsionWeb/src/main.ts
@@ -1,5 +1,4 @@
-// Import the main stylesheet to ensure all styles are bundled and applied.
-import '@src/styles/main.scss';
+import '@src/styles/main.scss'
 
 import { EngineBootstrap } from '@src/engineBootstrap'
 
